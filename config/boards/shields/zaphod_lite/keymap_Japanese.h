1f85cecf6761c4952f27435210a3ae8d5893b196
gilgameshone <91382279+gilgameshone@users.noreply.github.com>
2023-11-22 15:48:26 +0900

    try JIS

---
+++ config/boards/shields/zaphod_lite/keymap_Japanese.h
@@ -0,0 +1,75 @@
+#define JP_ZKHK &kp GRAVE  // Zenkaku ↔ Hankaku ↔ Kanji (半角 ↔ 全角 ↔ 漢字)
+#define JP_1    &kp N1    // 1
+#define JP_2    &kp N2    // 2
+#define JP_3    &kp N3    // 3
+#define JP_4    &kp N4    // 4
+#define JP_5    &kp N5    // 5
+#define JP_6    &kp N6    // 6
+#define JP_7    &kp N7    // 7
+#define JP_8    &kp N8    // 8
+#define JP_9    &kp N9    // 9
+#define JP_0    &kp N0    // 0
+#define JP_MINUS &kp MINUS // -
+#define JP_CARET &kp EQUAL   // ^
+#define JP_YEN  &kp INT3 // ¥
+#define JP_Q    &kp Q    // Q
+#define JP_W    &kp W    // W
+#define JP_E    &kp E    // E
+#define JP_R    &kp R    // R
+#define JP_T    &kp T    // T
+#define JP_Y    &kp Y    // Y
+#define JP_U    &kp U    // U
+#define JP_I    &kp I    // I
+#define JP_O    &kp O    // O
+#define JP_P    &kp P    // P
+#define JP_AT   &kp LBKT // @
+#define JP_LBKT &kp RBKT // [
+#define JP_EISU &kp CAPS // Eisū (英数)
+#define JP_A    &kp A    // A
+#define JP_S    &kp S    // S
+#define JP_D    &kp D    // D
+#define JP_F    &kp F    // F
+#define JP_G    &kp G    // G
+#define JP_H    &kp H    // H
+#define JP_J    &kp J    // J
+#define JP_K    &kp K    // K
+#define JP_L    &kp L    // L
+#define JP_SEMI &kp SEMI // ;
+#define JP_COLON &kp APOS // :
+#define JP_RBKT &kp NUHS // ]
+#define JP_Z    &kp Z    // Z
+#define JP_X    &kp X    // X
+#define JP_C    &kp C    // C
+#define JP_V    &kp V    // V
+#define JP_B    &kp B    // B
+#define JP_N    &kp N    // N
+#define JP_M    &kp M    // M
+#define JP_COMMA &kp COMMA // ,
+#define JP_DOT  &kp DOT  // .
+#define JP_SLASH &kp SLASH // /
+#define JP_BSLH &kp INT1 // (backslash)
+#define JP_MHEN &kp INT5 // Muhenkan (無変換)
+#define JP_HENK &kp INT4 // Henkan (変換)
+#define JP_KANA &kp INT2 // Katakana ↔ Hiragana ↔ Rōmaji (カタカナ ↔ ひらがな ↔ ローマ字)
+#define JP_EXCL LS(JP_1)    // !
+#define JP_DQT LS(JP_2)    // "
+#define JP_HASH LS(JP_3)    // #
+#define JP_DLLR  LS(JP_4)    // $
+#define JP_PRCNT LS(JP_5)    // %
+#define JP_AMPS LS(JP_6)    // &
+#define JP_APOS LS(JP_7)    // '
+#define JP_LPAR LS(JP_8)    // (
+#define JP_RPAR LS(JP_9)    // )
+#define JP_EQUAL  LS(JP_MINUS) // =
+#define JP_TILDE LS(JP_CARET) // ~
+#define JP_PIPE LS(JP_YEN)  // |
+#define JP_GRAVE  LS(JP_AT)   // `
+#define JP_LBRC LS(JP_LBKT) // {
+#define JP_CAPS LS(JP_EISU) // Caps Lock
+#define JP_PLUS LS(JP_SEMI) // +
+#define JP_ASTRK LS(JP_COLON) // *
+#define JP_LABK LS(JP_RBKT) // }
+#define JP_LT LS(JP_COMMA) // <
+#define JP_GT LS(JP_DOT)  // >
+#define JP_QMARK LS(JP_SLASH) // ?
+#define JP_UNDER LS(JP_BSLH) // _
